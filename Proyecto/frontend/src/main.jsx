c76ea488ee5ddd2a260294dbdee31951dc9c5e92
Nicolas <112877949+nzuluga@users.noreply.github.com>
2025-02-17 19:25:29 -0500

    Avance en el sistema de autenticación (#52)
    * Se adiciona trabajo de arquitectura de trabajo, para este caso se trabajo Django.
    
    * Avance en el sistema de autenticacion
    
    * Avance front (#42)
    
    * Se adiciona trabajo de arquitectura de trabajo, para este caso se trabajo Django.
    
    * Avance en el sistema de autenticacion
    
    * Modificaciones para que corra :v
    
    * Modificaciones para que corra :v :v
    
    * Se hace la adición del front para la recuperación de contraseña para así completar la sección de sistema de autenticación.
    
    * Center app content
    
    * Fix endOfLine linter warning in IntelliJ
    
    * Add material icons link ref
    
    * Fix input fields and submit button alignment
    
    * Add register route to NavBar
    
    * Add register route to the BrowserRouter
    
    * Create register view
    
    * Change endOfLine to use lf
    
    * Remove Navbar and Dashboard
    
    * Change lint endOfLine to crlf
    
    * Move tailwind import to beginning of file
    
    * Remove redundant style
    
    * * Fix line spaces
    * Fix imports
    * Change spans to buttons
    * Change buttons class name
    
    * Fix imports
    
    * Fix button styles collisions
    
    * * Add button prevent default
    
    * * Remove google material icons
    * Change icons to lucide-react
    
    * Change endOfLine to auto
    
    * Fix patient admission layout error
    
    ---------
    
    Co-authored-by: __init__ <aavendanoca@unal.edu.co>

--- Proyecto/frontend/src/main.jsx
+++ Proyecto/frontend/src/main.jsx
@@ -3,11 +3,12 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import { GlobalProvider } from "@/context/GlobalContext";
 import ToastContainer from "@/components/ToastContainer";
-
 import "@/styles/index.css";
 import Home from "@/pages/Home";
 import PatientAdmission from "@/pages/PatientAdmission";
 import Login from "@/pages/Login";
+import RecuperarPassword from "@/pages/RecuperarPassword";
+import Registro from "@/pages/Registro.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
@@ -19,6 +20,8 @@ createRoot(document.getElementById("root")).render(
             <Route path="patient-admission" element={<PatientAdmission />} />
           </Route>
           <Route path="/login" element={<Login />} />
+          <Route path="/recuperar" element={<RecuperarPassword />} />
+          <Route path="/registro" element={<Registro />} />
         </Routes>
         <ToastContainer />
       </BrowserRouter>
---
+++ Proyecto/frontend/src/pages/Registro.jsx
@@ -0,0 +1,94 @@
+import "@/styles/App.css";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react"; // Importamos los estilos
+import { Eye as EyeIcon, EyeOff as EyeOffIcon } from "lucide-react";
+
+function Registro() {
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    localStorage.removeItem("auth"); // Eliminar sesión
+    navigate("/"); // Redirigir a login
+  };
+
+  const [usuario, setUsuario] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+  const [email, setEmail] = useState("");
+  const [cedula, setCedula] = useState("");
+  const [passwordOcclude, setPasswordOcclude] = useState(true);
+
+  return (
+    <>
+      <div className="login-container">
+        <div className="login-box">
+          <h2 className="login-title">Registro de usuario</h2>
+          <form onSubmit={handleLogin}>
+            <input
+              type="text"
+              placeholder="Usuario"
+              value={usuario}
+              onChange={(e) => setUsuario(e.target.value)}
+            />
+            <br />
+            <input
+              type="number"
+              placeholder="Cedula"
+              value={cedula}
+              onChange={(e) => setCedula(e.target.value)}
+            />
+            <br />
+            <input
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <br />
+            <div className="password-container">
+              <input
+                type={passwordOcclude ? "password" : "text"}
+                placeholder="Contraseña"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button
+                className="material-icons eye-icon"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPasswordOcclude(!passwordOcclude);
+                }}
+              >
+                {passwordOcclude ? <EyeIcon /> : <EyeOffIcon />}
+              </button>
+            </div>
+            <br />
+            <div className="password-container">
+              <input
+                type={passwordOcclude ? "password" : "text"}
+                placeholder="Confirmar Contraseña"
+                value={passwordConfirm}
+                onChange={(e) => setPasswordConfirm(e.target.value)}
+              />
+              <button
+                className="eye-icon"
+                onClick={(e) => {
+                  e.preventDefault();
+                  setPasswordOcclude(!passwordOcclude);
+                }}
+              >
+                {passwordOcclude ? <EyeIcon /> : <EyeOffIcon />}
+              </button>
+            </div>
+            <br />
+            <button className={"submit-button"} type="submit">
+              Ingresar
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Registro;
