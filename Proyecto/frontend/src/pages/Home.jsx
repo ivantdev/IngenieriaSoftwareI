8c90bbc713ec57e80ff50b542e066a65dcfc365a
Ivan Castellanos <ivan@ivant.dev>
2025-02-23 12:51:12 -0500

    refactor: add user session validation and toast notification in Home component

--- Proyecto/frontend/src/pages/Home.jsx
+++ Proyecto/frontend/src/pages/Home.jsx
@@ -1,6 +1,20 @@
 import Dashboard from "@/components/Dashboard";
+import useGlobalContext from "@/hooks/useGlobalContext";
+import { useNavigate } from "react-router";
 
 function App() {
+  const navigate = useNavigate();
+  const { addToast, user } = useGlobalContext();
+
+  if (!user) {
+    return;
+  }
+
+  if (!user.isActiveSession) {
+    addToast("Debes iniciar sesión", "error");
+    navigate("/login");
+  }
+
   return <Dashboard />;
 }
 
