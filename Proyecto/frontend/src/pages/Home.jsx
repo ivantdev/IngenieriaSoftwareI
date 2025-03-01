37fa4ea1199c971483c90a4cc606f2964624282d
Ivan Castellanos <ivan@ivant.dev>
2025-03-01 18:44:39 -0500

    fix: component rendering

--- Proyecto/frontend/src/pages/Home.jsx
+++ Proyecto/frontend/src/pages/Home.jsx
@@ -1,18 +1,23 @@
 import Dashboard from "@/components/Dashboard";
 import useGlobalContext from "@/hooks/useGlobalContext";
 import { useNavigate } from "react-router";
+import { useEffect } from "react";
 
 function App() {
   const navigate = useNavigate();
   const { addToast, user } = useGlobalContext();
 
-  if (!user) {
-    return;
-  }
+  useEffect(() => {
+    if (!user) return;
 
-  if (!user.isActiveSession) {
-    addToast("Debes iniciar sesión", "error");
-    navigate("/login");
+    if (!user.isActiveSession) {
+      addToast("Debes iniciar sesión. Home", "error");
+      navigate("/login");
+    }
+  }, [user, navigate, addToast]);
+
+  if (!user || !user.isActiveSession) {
+    return null; // Evita renderizar el Dashboard antes de la navegación
   }
 
   return <Dashboard />;
