3725d47a449da776cd684e61d8f4f5203709cbc2
__init__ <aavendanoca@unal.edu.co>
2025-03-01 18:13:35 -0500

    Fix hook dependency error

--- Proyecto/frontend/src/hooks/useToast.jsx
+++ Proyecto/frontend/src/hooks/useToast.jsx
@@ -3,17 +3,14 @@ import { useState, useCallback } from "react";
 const useToast = () => {
   const [toasts, setToasts] = useState([]);
 
-  const addToast = useCallback(
-    (message, type = "info") => {
-      const id = crypto.randomUUID();
-      setToasts((prevToasts) => [...prevToasts, { id, message, type }]);
+  const addToast = useCallback((message, type = "info") => {
+    const id = crypto.randomUUID();
+    setToasts((prevToasts) => [...prevToasts, { id, message, type }]);
 
-      setTimeout(() => {
-        removeToast(id);
-      }, 5000);
-    },
-    [removeToast],
-  );
+    setTimeout(() => {
+      removeToast(id);
+    }, 5000);
+  }, []);
 
   const removeToast = useCallback((id) => {
     setToasts((prevToasts) => prevToasts.filter((toast) => toast.id !== id));
