97cf2da67376c879084f367bfdc7fcd3615b9a25
Ivan Castellanos <ivan@ivant.dev>
2025-03-02 16:04:50 -0500

    feat: configure static and media file handling with Whitenoise and update settings

--- Proyecto/django-backend/centro_medico/centro_medico/urls.py
+++ Proyecto/django-backend/centro_medico/centro_medico/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -12,3 +14,7 @@ urlpatterns = [
     path("api/", include("protocols.urls")),
     path("api/", include("notifications.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
