1f971313de80fb1f66eafc9a54363daa4a341677
Ivan Castellanos <ivan@ivant.dev>
2025-03-17 00:51:48 -0500

    add gunicorn to requirements and update WSGI configuration for shared path

--- Proyecto/django-backend/centro_medico/centro_medico/wsgi.py
+++ Proyecto/django-backend/centro_medico/centro_medico/wsgi.py
@@ -8,8 +8,13 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import sys
+from pathlib import Path
 from django.core.wsgi import get_wsgi_application
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent.parent
+sys.path.insert(0, str(BASE_DIR / "python_shared"))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "centro_medico.settings")
 
